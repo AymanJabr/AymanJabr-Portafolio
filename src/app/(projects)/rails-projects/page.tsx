4c4642bcce977478523b31ced4c7067b3c004aa6
AymanJabr <aymanjaber2012@gmail.com>
2025-06-09 09:49:55 +0300

    feat: update project pages, so that they have a common layout

---
+++ src/app/(projects)/rails-projects/page.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+
+const RailsProjects = () => {
+    return (
+        <div className="main-container">
+            <h3 style={{ textAlign: 'center' }}>Rails Projects</h3>
+
+            <div className="post-wrapper">
+                <div>
+                    <div className="post" style={{ height: '100%' }}>
+                        <Image className="thumbnail" src="/images/SocialMedia.JPG" alt="Social Media App" width={400} height={200} />
+                        <div className="post-preview">
+                            <h6 className="post-title">Social Media App</h6>
+                            <p className="post-intro">
+                                Rails implementation of a social media app, A user can create posts, other users can comment and like those posts, and users can become friends with each other in order to see each other's posts on their Timeline.
+                            </p>
+                            <a className="live-link" target="_blank" rel="noopener noreferrer" href="https://simplesocialmediabackend.herokuapp.com/">See Live</a>
+                            <a className="github-link" target="_blank" rel="noopener noreferrer" href="https://github.com/AymanJabr/Social-Media-Website-RubyOnRails">See Source</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="post" style={{ height: '100%' }}>
+                        <Image className="thumbnail" src="/images/workhours.JPG" alt="Workhours Tracker" width={400} height={200} />
+                        <div className="post-preview">
+                            <h6 className="post-title">Company Workhours Tracker</h6>
+                            <p className="post-intro">
+                                Rails website that lets you keep track of the workhours that have been put into a project. It shows who created the workhours, and it groups them by tasks. Users can see monthly reports of the project's progress.
+                            </p>
+                            <a className="live-link" target="_blank" rel="noopener noreferrer" href="https://project-workhours.herokuapp.com/">See Live</a>
+                            <a className="github-link" target="_blank" rel="noopener noreferrer" href="https://github.com/AymanJabr/Social-Media-Website-RubyOnRails">See Source</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RailsProjects; 
---
+++ src/app/(projects)/react-projects/page.tsx
@@ -0,0 +1,84 @@
+import Image from 'next/image';
+
+const ReactProjects = () => {
+    return (
+        <div className="main-container">
+            <h3 style={{ textAlign: 'center' }}>React Projects</h3>
+
+            <div className="post-wrapper">
+
+                <div>
+                    <div className="post" style={{ height: '100%' }}>
+                        <Image className="thumbnail" src="/images/covid-statistics.JPG" alt="COVID-19 Statistics" width={400} height={200} />
+                        <div className="post-preview">
+                            <h6 className="post-title">Worldwide COVID-19 Statistics</h6>
+                            <p className="post-intro">
+                                A Single Page Application, that makes use of the Corona-Tracker API. The application is updated bihourly with the worldwide and by country COVID-19 statistics.
+                            </p>
+                            <a className="live-link" target="_blank" rel="noopener noreferrer" href="https://worldwide-covid-statistics.netlify.app/">See Live</a>
+                            <a className="github-link" target="_blank" rel="noopener noreferrer" href="https://github.com/AymanJabr/Worldwide-Covid-Statistics">See Source</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="post" style={{ height: '100%' }}>
+                        <Image className="thumbnail" src="/images/game-of-life.JPG" alt="Game of Life" width={400} height={200} />
+                        <div className="post-preview">
+                            <h6 className="post-title">Game of Life</h6>
+                            <p className="post-intro">
+                                Implementation of the famous Conway's Game of Life.
+                            </p>
+                            <a className="live-link" target="_blank" rel="noopener noreferrer" href="https://game-of-life-ayman.netlify.app/">See Live</a>
+                            <a className="github-link" target="_blank" rel="noopener noreferrer" href="https://github.com/AymanJabr/Game-of-Life">See Source</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="post" style={{ height: '100%' }}>
+                        <Image className="thumbnail" src="/images/hangman.JPG" alt="React Hangman" width={400} height={200} />
+                        <div className="post-preview">
+                            <h6 className="post-title">React Hangman</h6>
+                            <p className="post-intro">
+                                Classical Hangman game, created using React.
+                            </p>
+                            <a className="live-link" target="_blank" rel="noopener noreferrer" href="https://my-react-hang-man.netlify.app/">See Live</a>
+                            <a className="github-link" target="_blank" rel="noopener noreferrer" href="https://github.com/AymanJabr/Hangman-React">See Source</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="post" style={{ height: '100%' }}>
+                        <Image className="thumbnail" src="/images/calculator.JPG" alt="React Calculator" width={400} height={200} />
+                        <div className="post-preview">
+                            <h6 className="post-title">React Calculator</h6>
+                            <p className="post-intro">
+                                React single-page app, that makes use of React Routing. In this project I created a simple calculator, with full functionality and testing.
+                            </p>
+                            <a className="live-link" target="_blank" rel="noopener noreferrer" href="https://simple-calculator-project.herokuapp.com/calculator">See Live</a>
+                            <a className="github-link" target="_blank" rel="noopener noreferrer" href="https://github.com/AymanJabr/React-Simple-Calculator">See Source</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="post" style={{ height: '100%' }}>
+                        <Image className="thumbnail" src="/images/bookstore.JPG" alt="Bookstore Library" width={400} height={200} />
+                        <div className="post-preview">
+                            <h6 className="post-title">Bookstore Library</h6>
+                            <p className="post-intro">
+                                React/Redux single-page app, that makes use of React Routing. In this project we created a simple bookstore app, with the ability to add books and to filter the available ones by category.
+                            </p>
+                            <a className="live-link" target="_blank" rel="noopener noreferrer" href="https://project-bookstore-cms.herokuapp.com/">See Live</a>
+                            <a className="github-link" target="_blank" rel="noopener noreferrer" href="https://github.com/AymanJabr/Bookstore-CMS">See Source</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReactProjects; 
