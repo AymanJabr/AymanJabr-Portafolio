a344f6c91c6d390c9f30de1bc0968155a069a3ed
AymanJabr <aymanjaber2012@gmail.com>
2025-06-13 09:41:38 +0300

    feat: merge react-projects and small-projects into small-projects-and-games

--- src/app/(projects)/small-projects-and-games/page.tsx
+++ src/app/(projects)/small-projects-and-games/page.tsx
@@ -1,11 +1,37 @@
 import ProjectPost from "@/app/components/ProjectPost";
 
-const SmallProjects = () => {
+const SmallProjectsAndGames = () => {
     return (
         <div className="main-container">
-            <h3 style={{ textAlign: 'center' }}>Small Projects</h3>
+            <h3 style={{ textAlign: 'center' }}>Small Projects and Games</h3>
 
             <div className="post-wrapper">
+                <ProjectPost
+                    title="Game of Life"
+                    imageSrc="/images/game-of-life.JPG"
+                    imageAlt="Game of Life"
+                    intro="Implementation of the famous Conway's Game of Life."
+                    liveLink="https://game-of-life-ayman.netlify.app/"
+                    sourceLink="https://github.com/AymanJabr/Game-of-Life"
+                />
+
+                <ProjectPost
+                    title="React Hangman"
+                    imageSrc="/images/hangman.JPG"
+                    imageAlt="React Hangman"
+                    intro="Classical Hangman game, created using React."
+                    liveLink="https://my-react-hang-man.netlify.app/"
+                    sourceLink="https://github.com/AymanJabr/Hangman-React"
+                />
+
+                <ProjectPost
+                    title="React Calculator"
+                    imageSrc="/images/calculator.JPG"
+                    imageAlt="React Calculator"
+                    intro="React single-page app, that makes use of React Routing. In this project I created a simple calculator, with full functionality and testing."
+                    liveLink="https://ayman-simple-calculator.netlify.app/"
+                    sourceLink="https://github.com/AymanJabr/React-Simple-Calculator"
+                />
                 <ProjectPost
                     title="Tic-Tac-Toe"
                     imageSrc="/images/Tic-Tac-Toe game.JPG"
@@ -46,17 +72,17 @@ const SmallProjects = () => {
                     liveLink="https://simple-platform-jumper.netlify.app/"
                     sourceLink="https://github.com/AymanJabr/Endless-Platformer-Phaser"
                 />
-                <ProjectPost
+                {/* <ProjectPost
                     title="Computer Speak."
                     imageSrc="/images/computerSpeak.JPG"
                     imageAlt="Computer Speak"
                     intro="A fun tool where you can make the computer say anything you want at different speeds."
                     liveLink="https://aymanjabr-computer-speak.netlify.app/"
                     sourceLink="https://github.com/AymanJabr/Text-To-Audio-JS-Speaker"
-                />
+                /> */}
             </div>
         </div>
     );
 };
 
-export default SmallProjects; 
+export default SmallProjectsAndGames; 
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -24,22 +24,6 @@ const Projects = () => {
                         linkText="See all Landing pages."
                     />
                     <ProjectCard
-                        imageSrc="/images/SocialMedia.JPG"
-                        imageAlt="Rails projects"
-                        title="Rails Projects"
-                        intro="A collection of websites that showcase some of the previous Rails projects that I have worked on solo or as a part of a team."
-                        linkHref="/rails-projects"
-                        linkText="See all Rails projects"
-                    />
-                    <ProjectCard
-                        imageSrc="/images/bookstore.JPG"
-                        imageAlt="React projects"
-                        title="React Projects"
-                        intro="A collection of simple projects and games done using React on the front-end, some of these projects are using an API as a backend."
-                        linkHref="/react-projects"
-                        linkText="See all React projects"
-                    />
-                    <ProjectCard
                         imageSrc="/images/theNextWeb.JPG"
                         imageAlt="Website clones"
                         title="Website Clones"
@@ -49,11 +33,11 @@ const Projects = () => {
                     />
                     <ProjectCard
                         imageSrc="/images/blackjack game.JPG"
-                        imageAlt="Small projects"
+                        imageAlt="Small projects and games"
                         title="Small Projects and Games"
-                        intro="A collection of small projects that have been built to showcase a particular skill or feature."
-                        linkHref="/small-projects"
-                        linkText="See all small projects"
+                        intro="A collection of small projects and games, some built with React, to showcase particular skills or features."
+                        linkHref="/small-projects-and-games"
+                        linkText="See all small projects and games"
                     />
                 </div>
             </div>
@@ -61,4 +45,4 @@ const Projects = () => {
     );
 };
 
-export default Projects; 
+export default Projects;
