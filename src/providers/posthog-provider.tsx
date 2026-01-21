916e7a0e173f8cbe2ff856930fdcbcbf5fb47008
AymanJabr <aymanjaber2012@gmail.com>
2026-01-21 21:08:51 +0300

    feat: add posthog provider

--- src/providers/posthog-provider.tsx
+++ src/providers/posthog-provider.tsx
@@ -1,9 +1,7 @@
 // app/providers.tsx
 'use client'
 
-import { usePathname, useSearchParams } from "next/navigation"
 import { useEffect } from "react"
-import { usePostHog } from 'posthog-js/react'
 
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
@@ -14,7 +12,6 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
       defaults: '2025-05-24',
-      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
       capture_pageleave: true
     })
   }, [])
